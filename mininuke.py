ba7fa6b1d7b86837b415e90de6dcea816ed48de5
Nathan <shweppsie@gmail.com>
2010-05-06 20:03:14 +1200

    refactoring

--- mininuke.py
+++ mininuke.py
@@ -13,36 +13,13 @@ import labels
 import configurator
 
 window = pyglet.window.Window(1024,768)
-#window = pyglet.window.Window(fullscreen=True)
 browse = browser.Browser(configurator.config.get("mininuke", "path"))
 selected = 0
 keys_pressed = set()
 nodes = []
 
-def update_list():
-    global nodes
-    nodes = browse.list()
-
-@window.event
-def on_key_press (symbol, modifiers):
-    keys_pressed.add(symbol)
-
-@window.event
-def on_key_release (symbol, modifiers):
-    if symbol in keys_pressed:
-        keys_pressed.remove(symbol)
-    keys_update()
-
-@window.event
-def on_mouse_motion(x, y, dx, dy):
-    if dy > 10
-        selected += 1 
-    if dy < -10
-        selected -= 1
-
-
 def keys_update():
-    global selected
+    global selected,nodes
     for i in keys_pressed:
         if i == key.Q:
             pyglet.app.exit() # q quits the program as does escape
@@ -58,41 +35,49 @@ def keys_update():
                 if browse.isdir(path):
                     selected = 0
                     browse.down(path)
-                    update_list()
+                    nodes = browse.list()
                 else:
                     filename = os.path.join(browse.getpath(),nodes[selected])
                     args = configurator.config.get("mplayer", "arguments")
                     player.Player(filename, args, configurator.config.get("mplayer", "log"))
         if i == key.BACKSPACE:
             browse.up()
-            update_list()
+            nodes = browse.list()
+
+
+@window.event
+def on_key_press (symbol, modifiers):
+    keys_pressed.add(symbol)
+
+@window.event
+def on_key_release (symbol, modifiers):
+    if symbol in keys_pressed:
+        keys_pressed.remove(symbol)
+    keys_update()
 
 @window.event
 def on_draw():
     keys_update()
     window.clear()
-    for i in listfiles():
-        i.draw()
-    title = labels.Title('MININUKE', x=window.width/2-80, y=window.height-60)
-    title.set_style('background_color', (0,0,0,255))
-    title.draw()
-    labels.Footnote('* indicates folders', x=window.width-225, y=20).draw()
-
-def listfiles():
-    drawlist = []
+    
     x = 100
     y = window.height/2 + (selected * 40)
+    
     for i in xrange(len(nodes)):
         if y > 200 and y < 550:
             if i is selected:
                 label = labels.Selected(nodes[i],x,y)
             else:
                 label = labels.Item(nodes[i],x,y)
-            drawlist.append(label)
+            label.draw()
         y -= 40
-    return drawlist
+    
+    title = labels.Title('MININUKE', x=window.width/2-80, y=window.height-60)
+    title.set_style('background_color', (0,0,0,255))
+    title.draw()
+    labels.Footnote('* indicates folders', x=window.width-225, y=20).draw()
 
-update_list()
+nodes = browse.list()
 
 pyglet.app.run()
 
