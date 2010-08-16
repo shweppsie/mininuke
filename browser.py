7d55980c8195332d3eef2fca9afd3e009c9d504e
Nathan Overall <shweppsie@gmail.com>
2010-08-16 21:07:26 +1200

    add image preview for files and folders

--- browser.py
+++ browser.py
@@ -2,76 +2,75 @@ import os
 import sys
 
 class Browser:
-    def __init__(self,path):
-        self.path = "/"
-        self.root = os.path.expanduser(path)
-        if not os.path.isdir(self.root):
-            raise IOError('Path does not exist '+self.root)
+	def __init__(self,path):
+		self.path = "/"
+		self.root = os.path.expanduser(path)
+		if not os.path.isdir(self.root):
+			raise IOError('Path does not exist '+self.root)
 
-    def getimage(self,path):
-        rootpath = os.path.join(self.getpath(),path)
-        if os.path.isfile(rootpath):
-            image = rootpath[:rootpath.rfind('.')]+'.png'
-            if os.path.exists(image):
-                return image
-            else:
-                return None
-        else:
-            rootpath = os.path.join(rootpath,'folder.png')
-            if os.path.exists(rootpath):
-                return rootpath 
+	def getimage(self,filename):
+	  	filename = os.path.join(self.getpath(),filename)
+		if os.path.isfile(filename):
+			image = filename[:filename.rfind('.')]+'.png'
+			if os.path.exists(image):
+				return image
+		directory = os.path.join(self.getpath(),filename,'folder.png')
+		print directory
+		if os.path.isfile(directory):
+			if os.path.exists(directory):
+				return directory
 
-    def getpath(self):
-        return os.path.join(self.root+self.path)
+	def getpath(self):
+		return os.path.join(self.root+self.path)
 
-    def isdir(self, path):
-        return os.path.isdir(os.path.join(self.getpath(),path))
+	def isdir(self, path):
+		return os.path.isdir(os.path.join(self.getpath(),path))
 
-    def down(self, dir):
-        newpath = os.path.join( self.getpath(),dir )
-        if os.path.isdir(newpath):
-            self.path = os.path.join( self.path,dir )
-        else:
-            raise IOError('Path does not exist '+newpath)
+	def down(self, dir):
+		newpath = os.path.join( self.getpath(),dir )
+		if os.path.isdir(newpath):
+			self.path = os.path.join( self.path,dir )
+		else:
+			raise IOError('Path does not exist '+newpath)
 
-    def up(self):
-        if not self.path == '/' or not self.path == '' or not self.path == '//':
-            self.path = os.path.dirname( self.path )
+	def up(self):
+		if not self.path == '/' or not self.path == '' or not self.path == '//':
+			self.path = os.path.dirname( self.path )
 
-    def curpath(self):
-        return self.path
+	def curpath(self):
+		return self.path
 
-    def list(self):
-        files = []
-        folders = []
+	def list(self):
+		files = []
+		folders = []
 
-        filter = ['avi','.mkv','divx','wmv','mov']
-        nodes = os.listdir( self.getpath() )
+		filter = ['avi','.mkv','divx','wmv','mov']
+		nodes = os.listdir( self.getpath() )
 
-        if nodes > 0:
-            for a in nodes:
-                if os.path.isdir(os.path.join(self.getpath(),a)):
-                    folders.append(a)
+		if nodes > 0:
+			for a in nodes:
+				if os.path.isdir(os.path.join(self.getpath(),a)):
+					folders.append(a)
 
-            for a in nodes:
-                for i in filter:
-                    if a.endswith(i):
-                        files.append(a)
-        
-        return (files, folders)
+			for a in nodes:
+				for i in filter:
+					if a.endswith(i):
+						files.append(a)
+		
+		return (files, folders)
 
 if __name__=="__main__":
-    print sys.argv
-    if len(sys.argv) != 2:
-        print "Usage: browser.py directory"
-        exit(1)
-    b = Browser(sys.argv[1])
-    print b.curpath()
-    b.down("tv")
-    print b.curpath()
-    b.down("American Dad")
-    print b.curpath()
-    b.down('Season 1')
-    print b.curpath()
-    b.up()
-    print b.curpath()
+	print sys.argv
+	if len(sys.argv) != 2:
+		print "Usage: browser.py directory"
+		exit(1)
+	b = Browser(sys.argv[1])
+	print b.curpath()
+	b.down("tv")
+	print b.curpath()
+	b.down("American Dad")
+	print b.curpath()
+	b.down('Season 1')
+	print b.curpath()
+	b.up()
+	print b.curpath()
--- mininuke.py
+++ mininuke.py
@@ -20,112 +20,132 @@ browse = browser.Browser(configurator.config.get("mininuke", "path"))
 keys_pressed = set()
 selected = 0
 nodes = []
+image = None
+
+def update_image():
+	global image
+	if len(nodes)>0:
+		imgpath = browse.getimage(nodes[selected][0])
+		if imgpath != None:
+			image = pyglet.image.load(imgpath)
+		else:
+			image = None
+	else:
+		image = None
 
 #updates the list of nodes (files and directories)
 def fillnodes():
-    global nodes
-    nodes = []
-    (files,folders) = browse.list()
-    for i in folders:
-        nodes.append((i,False))
-    for i in files:
-        nodes.append((i,True))
-    nodes = sorted(nodes, key=itemgetter(0))
+	global nodes
+	nodes = []
+	(files,folders) = browse.list()
+	for i in folders:
+		nodes.append((i,False))
+	for i in files:
+		nodes.append((i,True))
+	nodes = sorted(nodes, key=itemgetter(0))
 
 #keys that have action when held down go here
 def keys_update():
-    global selected,files,folders,keys_pressed
-    for i in keys_pressed:
-        if i == key.Q:
-            pyglet.app.exit() # q quits the program as does escape
-        if i == key.DOWN:
-            if selected < len(nodes)-1:
-                selected += 1
-        if i == key.UP:
-            if selected > 0:
-                selected -= 1
-        #page down and up take you to the next and previous character
-        if i == key.PAGEDOWN:
-            char = nodes[selected][0][0]
-            while selected + 1 < len(nodes):
-                selected += 1
-                if nodes[selected][0][0] != char:
-                    break
-        if i == key.PAGEUP:
-            char = nodes[selected][0][0]
-            while selected - 1 >= 0:
-                selected -= 1
-                if nodes[selected][0][0] != char:
-                    break
+	global selected,files,folders,keys_pressed
+	for i in keys_pressed:
+		if i == key.Q:
+			pyglet.app.exit() # q quits the program as does escape
+		if i == key.DOWN:
+			if selected < len(nodes)-1:
+				selected += 1
+				update_image()
+		if i == key.UP:
+			if selected > 0:
+				selected -= 1
+				update_image()
+		#page down and up take you to the next and previous character
+		if i == key.PAGEDOWN:
+			char = nodes[selected][0][0]
+			while selected + 1 < len(nodes):
+				selected += 1
+				if nodes[selected][0][0] != char:
+					update_image()
+					break
+		if i == key.PAGEUP:
+			char = nodes[selected][0][0]
+			while selected - 1 >= 0:
+				selected -= 1
+				if nodes[selected][0][0] != char:
+					update_image()
+					break
 
 #select current item
 def doitem(node):
-    global selected
-    if not node[1]:
-        #directory
-        browse.down(node[0])
-        selected = 0
-        fillnodes()
-    else:
-        #file
-        window.set_exclusive_mouse(False)
-        window.set_fullscreen(False)
-        player.Player(os.path.join(browse.getpath(),node[0]), configurator.config.get("mplayer", "arguments"), configurator.config.get("mplayer", "log"))
-        window.set_fullscreen(True)
-        window.set_exclusive_mouse(True)
-        window.activate()
-    
-        #ensure no more keypreses will be processed
-        keys_pressed = set() 
-        return
+	global selected
+	if not node[1]:
+		#directory
+		browse.down(node[0])
+		selected = 0
+		fillnodes()
+	else:
+		#file
+		window.set_exclusive_mouse(False)
+		window.set_fullscreen(False)
+		player.Player(os.path.join(browse.getpath(),node[0]), configurator.config.get("mplayer", "arguments"), configurator.config.get("mplayer", "log"))
+		window.set_fullscreen(True)
+		window.set_exclusive_mouse(True)
+		window.activate()
+	
+		#ensure no more keypreses will be processed
+		keys_pressed = set() 
+		return
 
 #key press event
 @window.event
 def on_key_press (symbol, modifiers):
-    global selected,keys_pressed
-    if symbol == key.ENTER or symbol == key.RIGHT:
-        if selected < len(nodes): # there must be files in this folder
-            doitem(nodes[selected])
-    elif symbol == key.BACKSPACE or symbol == key.LEFT:
-        browse.up()
-        fillnodes()
-        return
-    else:
-        keys_pressed.add(symbol)
+	global selected,keys_pressed
+	if symbol == key.ENTER or symbol == key.RIGHT:
+		if selected < len(nodes): # there must be files in this folder
+			doitem(nodes[selected])
+			update_image()
+	elif symbol == key.BACKSPACE or symbol == key.LEFT:
+		browse.up()
+		fillnodes()
+		update_image()
+		return
+	else:
+		keys_pressed.add(symbol)
 
 #unmark held down keys
 @window.event
 def on_key_release (symbol, modifiers):
-    if symbol in keys_pressed:
-        keys_pressed.remove(symbol)
-    keys_update()
+	if symbol in keys_pressed:
+		keys_pressed.remove(symbol)
+	keys_update()
 
 #draw method
 @window.event
 def on_draw():
-    keys_update()
-    window.clear()
-    
-    x = 100
-    y = (selected * 40) + (window.height/2)
-    #y = (window.height/2) - (selected * 40)
-    for i in xrange(len(nodes)):
-        if y > 100 and y < (window.height-160):
-            if nodes[i][1]:
-                label = labels.File(nodes[i][0],x,y)
-            else:
-                label = labels.Folder(nodes[i][0],x,y)
-            if i is selected:
-                label.set_style('bold',True)
-                label.set_style('italic',False)
-                label.font_size += 5
-            label.draw()
-        y -= 40
-    
-    title = labels.Title('MININUKE', x=window.width/2-80, y=(window.height-60) )
-    title.set_style('background_color', (0,0,0,255))
-    title.draw()
-    labels.Path(browse.curpath(), x=x, y=(window.height-120)).draw()
+	keys_update()
+	window.clear()
+	
+	x = 100
+	y = (selected * 40) + (window.height/2)
+	#y = (window.height/2) - (selected * 40)
+	for i in xrange(len(nodes)):
+		if y > 100 and y < (window.height-160):
+			if nodes[i][1]:
+				label = labels.File(nodes[i][0],x,y)
+			else:
+				label = labels.Folder(nodes[i][0],x,y)
+			if i is selected:
+				label.set_style('bold',True)
+				label.set_style('italic',False)
+				label.font_size += 5
+			label.draw()
+		y -= 40
+	
+	title = labels.Title('MININUKE', x=window.width/2-80, y=(window.height-60) )
+	title.set_style('background_color', (0,0,0,255))
+	title.draw()
+	labels.Path(browse.curpath(), x=x, y=(window.height-120)).draw()
+	if image != None:
+			image.blit((window.width/3)*2, window.height/2-image.height/2, 0)
 
 fillnodes()
 pyglet.app.run()
