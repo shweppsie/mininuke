667623cafda20980aed629095c81e44bd7e1dfa1
Nathan Overall <shweppsie@gmail.com>
2010-08-21 21:35:38 +1200

    Remove debug code

--- browser.py
+++ browser.py
@@ -16,7 +16,6 @@ class Browser:
 				return filename
 		elif os.path.isdir(full_path):
 			directory = os.path.join(self.root,self.path,path,'folder.png')
-			print directory
 			if os.path.exists(directory):
 				return directory
 
